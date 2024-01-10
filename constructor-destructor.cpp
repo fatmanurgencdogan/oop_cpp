e7758574f7b1cdc43374a6e865fe9a5f965411b6
metegencdogan <metegencdogan@gmail.com>
2024-01-10 19:50:44 +0100

    Format code

--- constructor-destructor.cpp
+++ constructor-destructor.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
 using namespace std;
 
-class myclass {
-    private:
-        int a;
-    public:
-        myclass();//constructor
-        ~myclass();//destructor
-        void set_a(int num);
-        int get_a();
+class myclass
+{
+private:
+    int a;
 
+public:
+    myclass();  // constructor
+    ~myclass(); // destructor
+
+    void set_a(int num);
+    int get_a();
 };
-void myclass::set_a(int num){
-a=num;
+void myclass::set_a(int num)
+{
+    a = num;
 }
 
-int myclass::get_a(){
-return a;
+int myclass::get_a()
+{
+    return a;
 }
 
-myclass::myclass(){
-    cout<< "In Constructor"<<endl;
-    }
-myclass::~myclass(){
-    cout<<"In Destructor"<<endl;
-    }
+myclass::myclass()
+{
+    cout << "In Constructor" << endl;
+}
+myclass::~myclass()
+{
+    cout << "In Destructor" << endl;
+}
 
-int main(){
+int main()
+{
+    myclass object_1;
+    myclass object_2;
 
-myclass object_1,object_2;
-object_1.set_a(15);
-object_2.set_a(22);
+    object_1.set_a(15);
+    object_2.set_a(22);
 
-return 0;
-
+    return 0;
 }
